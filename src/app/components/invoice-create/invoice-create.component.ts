1403daf5d01ebf330953fb38d92a257a8cc469cb
Omaremad2900 <omaremad2900@gmail.com>
2024-09-12 15:46:36 +0300

    fixing the styling to ensure consistency

--- src/app/components/invoice-create/invoice-create.component.ts
+++ src/app/components/invoice-create/invoice-create.component.ts
@@ -1,33 +1,43 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Invoice } from '../../interfaces/invoice';
 import { InvoiceService } from '../../services/invoice.service';
 import { Router } from '@angular/router';
-import { FormsModule } from '@angular/forms';
+import { FormBuilder, FormGroup, Validators, FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-invoice-create',
   standalone: true,
-  imports: [CommonModule, FormsModule],
+  imports: [CommonModule, FormsModule, ReactiveFormsModule],
   templateUrl: './invoice-create.component.html',
-  styleUrl: './invoice-create.component.css'
+  styleUrls: ['./invoice-create.component.css']
 })
-export class InvoiceCreateComponent {
-  invoice: Invoice = {
-    patientId: 0,
-    appointmentId: 0,
-    amount: 0,
-    status: '',
-    createdAt: '',
-    updatedAt: ''
-  };
+export class InvoiceCreateComponent implements OnInit {
+  createForm!: FormGroup;
 
-  constructor(private invoiceService: InvoiceService, private router: Router) { }
+  constructor(
+    private invoiceService: InvoiceService, 
+    private router: Router,
+    private _formBuilder: FormBuilder
+  ) { }
 
-  createInvoice(): void {
-    this.invoiceService.createInvoice(this.invoice).subscribe(() => {
-      this.router.navigate(['/invoices']);
+  ngOnInit(): void {
+    this.createForm = this._formBuilder.group({
+      patientId: [0, [Validators.required, Validators.min(1)]],
+      appointmentId: [0, [Validators.required, Validators.min(1)]],
+      amount: [0, [Validators.required, Validators.min(1)]],
+      status: ['', [Validators.required, Validators.minLength(3)]],
+      createdAt: [''],
+      updatedAt: ['']
     });
   }
 
+  createInvoice(): void {
+    if (this.createForm.valid) {
+      const newInvoice: Invoice = this.createForm.value;
+      this.invoiceService.createInvoice(newInvoice).subscribe(() => {
+        this.router.navigate(['/invoices']);
+      });
+    }
+  }
 }
